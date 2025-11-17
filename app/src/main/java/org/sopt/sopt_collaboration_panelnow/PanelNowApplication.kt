af6eafffe6a9f70cda34bac403b2dd902f98a13f
Son Juwan <wndhks171@naver.com>
2025-11-17 22:49:47 +0900

    init/#1: 패키지 분리

---
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/PanelNowApplication.kt
@@ -0,0 +1,11 @@
+package org.sopt.sopt_collaboration_panelnow
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PanelNowApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/MainScreen.kt
@@ -0,0 +1,14 @@
+package org.sopt.sopt_collaboration_panelnow.presentation.main
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "MainScreen"
+    )
+}
