0fac124ea98a57ee69fd2a9499eb46e541133461
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-21 00:49:09 +0900

    feature/#4: Route 경로 설정
    - 네비게이터에서 사용하는 경로 sealed class를 이용해 설정

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/navigation/Route.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/navigation/Route.kt
@@ -1,9 +1,14 @@
-package org.sopt.sopt_collaboration_panelnow.presentation.main
+package org.sopt.sopt_collaboration_panelnow.presentation.main.navigation
 
 sealed class Route(val path: String) {
+
     object Survey : Route("survey")
+
     object Event : Route("event")
+
     object Home : Route("home")
+
     object Exchange : Route("exchange")
+
     object MyAction : Route("myAction")
 }
