57e98919d5c42e1037e4c58a0d377d3fa4bb1d74
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-24 03:15:03 +0900

    feature/#17: CustomButton 구현
    교환하기 및 다른 곳에서 사용되는 커스텀 버튼 구현

---
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/home/component/CustomButton.kt
@@ -0,0 +1,47 @@
+package org.sopt.sopt_collaboration_panelnow.presentation.home.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.Gray4
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.MainBlue
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.White
+
+
+@Composable
+fun CustomButton(onClick: () -> Unit, modifier: Modifier = Modifier, text: String) {
+    Button(
+        onClick = onClick, modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(
+            topStart = 15.dp,
+            topEnd = 999.dp,
+            bottomEnd = 999.dp,
+            bottomStart = 0.dp
+        ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MainBlue,
+            contentColor = White
+        )
+    ) { Text(text = text, style = TextStyle(White)) }
+}
+
+@Preview("ButtonPreview")
+@Composable
+private fun CustomButtonPreview() {
+    Surface(color = Gray4) {
+        CustomButton(onClick = {}, modifier = Modifier, text = "교환하기")
+    }
+}
