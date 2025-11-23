274c8368b5ca7a4fdf78d589152ec2527eca15ea
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-24 05:44:01 +0900

    feature/#17: 미니테스트 더미데이터 작성
    - 이것도 SurveyList와 마찬가지로 적절한 경로를 정하지 못해 우선적으로 모델 파일에 같이 작성 추후 리팩토링 예정

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/home/model/MiniTest.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/home/model/MiniTest.kt
@@ -6,3 +6,21 @@ data class MiniTestModel(
     val seconds: Int,
     val points: Int,
 )
+
+private val miniTestList = listOf(
+    MiniTestModel(
+        title = "겨울에도 아이스 아메리카노를 드시나요?",
+        seconds = 50,
+        points = 25
+    ),
+    MiniTestModel(
+        title = "요즘 당신의 최애 간식은 무엇인가요?",
+        seconds = 50,
+        points = 25
+    ),
+    MiniTestModel(
+        title = "떠나고 싶으신 곳이 있으신가요?",
+        seconds = 50,
+        points = 25
+    )
+)
