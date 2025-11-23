0feb734386132f3851a6fca85c1c48eb22582746
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-24 03:39:51 +0900

    feature/#17: PointLabel 구현
    인기 설문 섹션, 미니 테스트 섹션에 사용되는 포인트 라벨 컴포넌트로 구현

---
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/home/component/PointLabel.kt
@@ -0,0 +1,41 @@
+package org.sopt.sopt_collaboration_panelnow.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.MainBlue
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.SubBlue
+
+
+
+@Composable
+fun PointLabel(points: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .width(77.dp)
+
+            .height(38.dp)
+            .background(SubBlue, CircleShape),
+        contentAlignment = Alignment.Center
+    ) { Text(text = points, modifier.fillMaxWidth().padding(3.dp),color = MainBlue, fontWeight = FontWeight.Bold, fontSize = 18.sp, textAlign = TextAlign.Center) }
+
+}
+
+@Preview
+@Composable
+private fun PointLabelPreview() {
+    PointLabel(points = "230P")
+}
