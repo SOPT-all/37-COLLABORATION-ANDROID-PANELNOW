4d744ea106a3d03358401b30734d2e01b40623e5
Yerim Son <sonyerim12@gmail.com>
2025-11-23 23:48:03 +0900

    feat/#15: ProductCard 컴포넌트 구현

---
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/pointexchange/component/ProductCard.kt
@@ -0,0 +1,70 @@
+package org.sopt.sopt_collaboration_panelnow.presentation.pointexchange.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.PanelNowTheme
+
+@Composable
+fun ProductCard(
+    imageUrl: String,
+    title: String,
+    businessDays: Int,
+    points: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .width(160.dp)
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = title,
+            modifier = Modifier
+                .width(160.dp)
+                .height(116.dp),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = title,
+            style = PanelNowTheme.typography.titleM14,
+            color = PanelNowTheme.colors.gray6,
+            modifier = Modifier.width(125.dp)
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            text = "$businessDays 영업일 소요",
+            style = PanelNowTheme.typography.titleM12,
+            color = PanelNowTheme.colors.gray1,
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "%,dP".format(points),
+            style = PanelNowTheme.typography.titleSb16,
+            color = PanelNowTheme.colors.gray6,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MyPointCardPreview() {
+    PanelNowTheme {
+        ProductCard(
+            imageUrl = "",
+            title = "네이버페이 포인트쿠폰 3000원권",
+            businessDays = 3,
+            points = 3200,
+        )
+    }
+}
