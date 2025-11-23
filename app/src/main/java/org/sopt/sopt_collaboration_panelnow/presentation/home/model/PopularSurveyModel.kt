0c7127b846c08400fb1688705569dc88ad0ba749
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-24 05:17:14 +0900

    feature/#17: 인기설문 더미데이터 생성
    - 현재 더미데이터와 모델을 어디에 두는게 적절한지 모르겠어서,일단 같은 파일에 생성 추후 리팩토링 예정

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/home/model/PopularSurveyModel.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/home/model/PopularSurveyModel.kt
@@ -1,5 +1,29 @@
 package org.sopt.sopt_collaboration_panelnow.presentation.home.model
 
 import androidx.annotation.DrawableRes
+import org.sopt.sopt_collaboration_panelnow.R
 
-data class PopularSurveyModel(val title: String, val minutes :Int, val points : Int, @DrawableRes val iconsRes : Int)
+data class PopularSurveyModel(val title: String, val minutes :Int, val points : Int, @DrawableRes val iconRes : Int)
+
+val popularSurveyPreviewList = listOf(
+    PopularSurveyModel(
+
+        title = "블랙프라이데이 쇼핑 경험",
+      minutes = 1,
+       points = 100,
+        iconRes = R.drawable.ic_point_card
+    ),
+    PopularSurveyModel(
+
+        title = "운동을 즐겨 하시나요?",
+        minutes = 15,
+        points = 230,
+        iconRes = R.drawable.ic_point_exercise
+    ),
+    PopularSurveyModel(
+
+        title = "어느 지역에서 생활하시나요?",
+        minutes = 23,
+        points = 500,
+        iconRes= R.drawable.ic_point_map),
+)
