09f929e8a9e19bbd597772a335807ac345a18477
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-24 05:11:30 +0900

    feature/#17: 인기설문 모델 구현

---
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/home/model/PopularSurveyModel.kt
@@ -0,0 +1,5 @@
+package org.sopt.sopt_collaboration_panelnow.presentation.home.model
+
+import androidx.annotation.DrawableRes
+
+data class PopularSurveyModel(val title: String, val minutes :Int, val points : Int, @DrawableRes val iconsRes : Int)
