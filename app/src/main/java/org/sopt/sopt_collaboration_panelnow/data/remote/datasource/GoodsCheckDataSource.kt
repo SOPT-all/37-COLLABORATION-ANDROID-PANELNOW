1d932f96c577f3ef2a7e972703078ea89abe0b3d
Son Juwan <wndhks171@naver.com>
2025-11-25 18:51:19 +0900

    feat/#24: 상품 교환 API - datasource

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/data/remote/datasource/GoodsCheckDataSource.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/data/remote/datasource/GoodsCheckDataSource.kt
@@ -2,7 +2,9 @@ package org.sopt.sopt_collaboration_panelnow.data.remote.datasource
 
 import org.sopt.sopt_collaboration_panelnow.data.remote.dto.BaseResponse
 import org.sopt.sopt_collaboration_panelnow.data.remote.dto.response.GoodsCheckResponse
+import org.sopt.sopt_collaboration_panelnow.data.remote.dto.response.PurchaseResponse
 
 interface GoodsCheckDataSource {
     suspend fun getGoodsCheck(productId: Int): BaseResponse<GoodsCheckResponse>
+    suspend fun postPurchase(userId: Long, productId: Int): BaseResponse<PurchaseResponse>
 }
