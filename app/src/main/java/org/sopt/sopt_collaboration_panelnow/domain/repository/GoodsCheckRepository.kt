d11a9569bd126a38855710b06debcf5a463c339f
Son Juwan <wndhks171@naver.com>
2025-11-25 18:51:00 +0900

    feat/#24: 상품 교환 API -repository

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/domain/repository/GoodsCheckRepository.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/domain/repository/GoodsCheckRepository.kt
@@ -3,7 +3,6 @@ package org.sopt.sopt_collaboration_panelnow.domain.repository
 import org.sopt.sopt_collaboration_panelnow.domain.entity.GoodsCheck
 
 interface GoodsCheckRepository {
-    suspend fun getGoodsCheck(
-        productId: Int,
-    ): Result<GoodsCheck>
+    suspend fun getGoodsCheck(productId: Int): Result<GoodsCheck>
+    suspend fun postPurchase(userId: Long, productId: Int): Result<String>
 }
