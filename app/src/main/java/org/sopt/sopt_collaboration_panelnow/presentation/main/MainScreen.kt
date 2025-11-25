1f812fa744fb634f4b550cac7e87bdf84712a506
Son Juwan <wndhks171@naver.com>
2025-11-25 18:50:35 +0900

    feat/#24: Navigation/MainScreen 연결

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/MainScreen.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/MainScreen.kt
@@ -1,14 +1,66 @@
 package org.sopt.sopt_collaboration_panelnow.presentation.main
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import org.sopt.sopt_collaboration_panelnow.presentation.main.navigation.*
 
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = "MainScreen"
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentDestination: Route? = navBackStackEntry?.destination?.route?.let { route ->
+        when {
+            route.contains("Home") -> Home
+            route.contains("Survey") -> Survey
+            route.contains("Event") -> Event
+            route.contains("Exchange") -> Exchange
+            route.contains("MyAction") -> MyAction
+            else -> null
+        }
+    }
+
+    val items = listOf(
+        NavigationItem.SurveyItem,
+        NavigationItem.EventItem,
+        NavigationItem.HomeItem,
+        NavigationItem.ExchangeItem,
+        NavigationItem.MyActionItem
     )
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        bottomBar = {
+            CustomNavigatorBar(
+                items = items,
+                centerIndex = items.indexOf(NavigationItem.HomeItem),
+                currentRoute = currentDestination,
+                onItemClick = { item ->
+                    if (currentDestination == item.route) {
+                        return@CustomNavigatorBar
+                    }
+
+                    navController.navigate(item.route) {
+                        popUpTo(Home) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        NaviHost(
+            navController = navController,
+            innerPadding = padding
+        )
+    }
 }
