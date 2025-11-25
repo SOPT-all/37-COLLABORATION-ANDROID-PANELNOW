b35d083b9a9cc469a2973a43f3060e27af511852
Yerim Son <sonyerim12@gmail.com>
2025-11-26 00:19:01 +0900

    feat/#21: domain/GoodsCheck

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/domain/repository/GoodsCheckRepository.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/domain/repository/GoodsCheckRepository.kt
@@ -1,8 +1,10 @@
 package org.sopt.sopt_collaboration_panelnow.domain.repository
 
 import org.sopt.sopt_collaboration_panelnow.domain.entity.GoodsCheck
+import org.sopt.sopt_collaboration_panelnow.domain.entity.Product
 
 interface GoodsCheckRepository {
+    suspend fun getProducts(sort: String): Result<List<Product>>
     suspend fun getGoodsCheck(productId: Int): Result<GoodsCheck>
     suspend fun postPurchase(userId: Long, productId: Int): Result<String>
 }
