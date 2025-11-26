8453f313fb92d531c0175af37217253779ceaef2
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-26 20:30:20 +0900

    fix/#22: AppBar 아이콘 수정

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/navigation/NaviHost.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/navigation/NaviHost.kt
@@ -5,16 +5,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.wear.compose.material.Scaffold
-import androidx.wear.compose.material.Text
+import org.sopt.sopt_collaboration_panelnow.R
 import org.sopt.sopt_collaboration_panelnow.core.designsystem.component.PanelNowTopBar
 import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.PanelNowTheme
-import org.sopt.sopt_collaboration_panelnow.presentation.home.model.MiniTestModel
 import org.sopt.sopt_collaboration_panelnow.presentation.home.model.miniTestList
 import org.sopt.sopt_collaboration_panelnow.presentation.home.screen.HomeScreen
 import org.sopt.sopt_collaboration_panelnow.presentation.pointdetail.PointDetailRoute
@@ -38,27 +40,26 @@ fun NaviHost(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(PanelNowTheme.colors.gray4)
-            ){
-                PanelNowTopBar(
-                    content = {
-                        Text(
-                            text = "패널나우",
-                            style = PanelNowTheme.typography.titleSb16,
-                            color = PanelNowTheme.colors.gray6
-                        )
-                    },
-                    onAlarmClick = {  },
-                    onProfileClick = {  }
-
-                )
+            ) {
+                PanelNowTheme {
+                    PanelNowTopBar(
+                        content = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.logo),
+                                contentDescription = stringResource(R.string.top_bar_logo),
+                                tint = Color.Unspecified,
+                            )
+                        }
+                    )
+                }
                 HomeScreen(
-                    pointsText = "4,500P",
                     miniTests = miniTestList
                 )
 
             }
 
-        } // 이 안에 각자 스크린 넣으시면 됩니다
+        }
+        // 이 안에 각자 스크린 넣으시면 됩니다
         composable<Survey> { }
         composable<Event> { }
         composable<Exchange> {
