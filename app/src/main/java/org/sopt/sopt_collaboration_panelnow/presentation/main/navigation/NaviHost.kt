d143376c8c2e2b09b99d67cdb7ef0d80b25e5dab
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-26 14:54:45 +0900

    feature/#22: AppBar 적용

--- app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/navigation/NaviHost.kt
+++ app/src/main/java/org/sopt/sopt_collaboration_panelnow/presentation/main/navigation/NaviHost.kt
@@ -1,5 +1,7 @@
 package org.sopt.sopt_collaboration_panelnow.presentation.main.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,6 +10,10 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.Text
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.component.PanelNowTopBar
+import org.sopt.sopt_collaboration_panelnow.core.designsystem.theme.PanelNowTheme
 import org.sopt.sopt_collaboration_panelnow.presentation.home.model.MiniTestModel
 import org.sopt.sopt_collaboration_panelnow.presentation.home.model.miniTestList
 import org.sopt.sopt_collaboration_panelnow.presentation.home.screen.HomeScreen
@@ -28,10 +34,30 @@ fun NaviHost(
             .fillMaxSize()
     ) {
         composable<Home> {
-            HomeScreen(
-                pointsText = "4,500P",
-                miniTests = miniTestList
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(PanelNowTheme.colors.gray4)
+            ){
+                PanelNowTopBar(
+                    content = {
+                        Text(
+                            text = "패널나우",
+                            style = PanelNowTheme.typography.titleSb16,
+                            color = PanelNowTheme.colors.gray6
+                        )
+                    },
+                    onAlarmClick = {  },
+                    onProfileClick = {  }
+
+                )
+                HomeScreen(
+                    pointsText = "4,500P",
+                    miniTests = miniTestList
+                )
+
+            }
+
         } // 이 안에 각자 스크린 넣으시면 됩니다
         composable<Survey> { }
         composable<Event> { }
